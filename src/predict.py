6e2c6d383f8b027d23d50f927fbd580b29cbf0cb
Benjamin Cretois <benjamin.cretois@nina.no>
2024-07-25 11:08:29 +0200

    [FIX] calculate HR when snowmobile detection

--- src/predict.py
+++ src/predict.py
@@ -17,10 +17,10 @@ import logging
 # Open the config file
 import yaml
 from yaml import FullLoader
-with open("../CONFIG.yaml") as f:
+with open("./CONFIG.yaml") as f:
     cfg = yaml.load(f, Loader=FullLoader)
 
-logging.basicConfig(filename='../logs/logfile.log', level=logging.INFO,
+logging.basicConfig(filename='./logs/logfile.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 def initModel(model_path, device):
@@ -42,7 +42,7 @@ def compute_hr(array):
 
     return hr
 
-def predict(testLoader, model, device, threshold=0.99):
+def predict(testLoader, model, device):
 
     proba_list = []
     hr_list = []
@@ -60,9 +60,10 @@ def predict(testLoader, model, device, threshold=0.99):
         output = np.exp(output.cpu().detach().numpy())
         proba_list.append(output[0])
 
-        # Compute HR if confidence is more than a threshold
-        max_value = output[0].max()
-        if max_value >= threshold:
+        # Compute HR if label=snowmobile
+        label = np.argmax(output[0], axis=0)
+
+        if label == 1:
             hr = compute_hr(np.array(array))
             hr_list.append(hr)
         else:
@@ -70,27 +71,7 @@ def predict(testLoader, model, device, threshold=0.99):
 
     return proba_list, hr_list
 
-
-def get_outname(input, out_path):
-
-    # Get a name for the output // if there are multiple "." in the list
-    # only remove the extension
-    filename = input.split("/")[-1].split(".")
-    if len(filename) > 2:
-        filename = ".".join(filename[0:-1])
-    else:
-        filename = input.split("/")[-1].split(".")[0]
-
-    # Make folder if it doesn't exist
-    outpath = os.sep.join([out_path, os.path.dirname(input)])
-    if not os.path.exists(outpath):
-        os.makedirs(outpath, exist_ok=True)
-
-    file_path = os.path.join(outpath, filename + ".csv")
-
-    return file_path
-
-def write_results(prob_audioclip_array, hr_array, outname, min_hr, min_conf):
+def write_results(prob_audioclip_array, hr_array, outname):
 
     # Store the array result in a CSV friendly format
     rows_for_csv = []
@@ -105,10 +86,9 @@ def write_results(prob_audioclip_array, hr_array, outname, min_hr, min_conf):
         max_value = conf.max()
         hr = np.array(item_hr)
 
-        # If the label is not "soundscape" then write the row:
-        if label != 0 and hr > min_hr and max_value > min_conf:
-            item_properties = [idx_begin, idx_end, label, max_value, hr]
-            rows_for_csv.append(item_properties)
+        # Write everything in the row
+        item_properties = [idx_begin, idx_end, label, max_value, hr]
+        rows_for_csv.append(item_properties)
 
         # Update the start time of the detection
         idx_begin = idx_end
@@ -130,7 +110,7 @@ def write_results(prob_audioclip_array, hr_array, outname, min_hr, min_conf):
 
 
 def analyzeFile(
-    file_path, model, device, num_workers, min_hr, min_conf, batch_size=1
+    file_path, model, device, num_workers, batch_size=1
 ):
     # Start time
     start_time = datetime.datetime.now()
@@ -143,27 +123,24 @@ def analyzeFile(
         os.makedirs(result_folder)
 
     # Check if the output already exists
-    outname = file_path.split("/")[-1].split(".")[0] + "_ANALYZED.csv"
+    outname = file_path.split("/")[-1] + "_ANALYZED.csv"
     outpath = os.path.join(result_folder, outname)
 
-    if os.path.exists(outname):
-        print("File {} already exists".format(outname))
-    else:
-        # Run the predictions
-        list_preds = AudioList().get_processed_list(file_path)
-        predLoader = DataLoader(
-            list_preds, batch_size=batch_size, num_workers=num_workers, pin_memory=False
-        )
+    # Run the predictions
+    list_preds = AudioList().get_processed_list(file_path)
+    predLoader = DataLoader(
+        list_preds, batch_size=batch_size, num_workers=num_workers, pin_memory=False
+    )
 
-        pred_audioclip_array, pred_hr_array = predict(predLoader, model, device)
+    pred_audioclip_array, pred_hr_array = predict(predLoader, model, device)
 
-        write_results(pred_audioclip_array, pred_hr_array, outpath, min_hr, min_conf)
+    write_results(pred_audioclip_array, pred_hr_array, outpath)
 
-        # Give the time it took to analyze file
-        delta_time = (datetime.datetime.now() - start_time).total_seconds()
-        message = "Finished {} in {:.2f} seconds".format(file_path, delta_time)
-        print(message, flush=True)
-        logging.info(message)
+    # Give the time it took to analyze file
+    delta_time = (datetime.datetime.now() - start_time).total_seconds()
+    message = "Finished {} in {:.2f} seconds".format(file_path, delta_time)
+    print(message, flush=True)
+    logging.info(message)
 
 def main(filename, cfg):
 
@@ -174,8 +151,6 @@ def main(filename, cfg):
                 model, 
                 device, 
                 num_workers=cfg["NUM_WORKERS"],
-                min_hr=cfg["MIN_HR"],
-                min_conf=cfg["MIN_CONF"],
                 batch_size=cfg["BATCH_SIZE"])
 
 
